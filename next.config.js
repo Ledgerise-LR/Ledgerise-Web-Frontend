bb5276fa277431d54761825fee9695b86ed93b2f
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-03-05 23:08:22 +0300

    .

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   images: {
     loader: "akamai",
-    path: ""
+    path: "/public"
   }
 }
 
