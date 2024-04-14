6e57a3839e49112c883f9e0d86eeebf0e158b261
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-04-14 21:41:50 +0300

    .

--- components/ApiTab.jsx
+++ components/ApiTab.jsx
@@ -5,8 +5,8 @@ export default function ApiTab({ title, method }) {
       <div>{title}</div>
       {
         method == "GET"
-          ? <div className='bg-green-700 text-slate-50 text-xxs flex justify-between items-center px-3 py-0.5 rounded-full'>GET</div>
-          : <div className='bg-blue-600 text-slate-50 text-xxs flex justify-between items-center px-3 py-0.5 rounded-full'>POST</div>
+          ? <div style={{fontSize: "10px"}} className='bg-green-700 text-slate-50 text-xxs flex justify-between items-center px-3 py-0.5 rounded-full'>GET</div>
+          : <div style={{fontSize: "10px"}} className='bg-blue-600 text-slate-50 text-xxs flex justify-between items-center px-3 py-0.5 rounded-full'>POST</div>
       }
     </div>
   )
