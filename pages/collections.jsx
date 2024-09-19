430f506770db7ad9d365edd79b8691ff567f73b9
harundmrpnce <harundemirpence@gmail.com>
2024-09-19 12:49:04 +0300

    Improve design

--- pages/collections.jsx
+++ pages/collections.jsx
@@ -1,66 +1,62 @@
-import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'
-import { useMoralis, useWeb3Contract } from 'react-moralis'
-import networkMapping from "../constants/networkMapping.json";
-import { getEthToUsdRate } from '@/utils/getEthToUsdRate';
+import { useState, useEffect } from 'react';
 import { URL, PORT } from '@/serverConfig';
 import axios from 'axios';
-
+import { FaSpinner } from 'react-icons/fa';
 
 export default function Home() {
-
-  const { isWeb3Enabled, chainId } = useMoralis();
-
+  const [loading, setLoading] = useState(true);
   const [collections, setCollections] = useState([]);
 
   useEffect(() => {
-    axios.get(`${URL}:${PORT}/subcollection/get-all-collections`)
-      .then(res => {
+    const fetchCollections = async () => {
+      try {
+        const res = await axios.get(`${URL}:${PORT}/subcollection/get-all-collections`);
         const data = res.data;
         setCollections(data.subcollections);
-      })
-  }, [])
+      } catch (error) {
+        console.error("Error fetching collections:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  const chainString = chainId ? parseInt(chainId, 16).toString() : "5";
-  const marketplaceAddress = networkMapping["Marketplace"][chainString];
+    fetchCollections();
+  }, []);
 
   return (
-    <div>
-      <div className='text-xl mt-4 w-full px-12 flex justify-between items-center'>
-        <div className='text-gray-700'>Bağış kampanyaları</div>
-        <div className='text-sm text-gray-700'>{collections ? collections.length : ("")} adet</div>
+    <>
+      <div className='w-full text-2xl flex flex-col items-center text-center max-md:pt-16 my-12'>
+        <div>Bağış kampanyalarını keşfet</div>
+        <div>Nasıl destek olabileceğini gör!</div>
       </div>
-      <div className='w-full border-b my-4'></div>
-      <div className='px-8 mb-20 w-full flex flex-1 flex-wrap'>
-        {!collections
-          ? (<div>Loading...</div>)
+      <div className='px-4 xs:px-6 sm:px-8 md:px-10 lg:px-12 mb-5 md:mb-10 xl:mb-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
+        {loading
+          ? (
+            <div className='w-full flex justify-center items-center col-span-full'>
+              <FaSpinner className='animate-spin text-2xl' />
+            </div>
+          )
           : (
-            collections.map((collection) => {
-              return (
-                <a className='mr-4 w-96 mb-12 bg-white hover:brightness-90 transition-all' href={`/collection?id=${collection.itemId}&nftAddress=${collection.nftAddress}`}>
-                  <div className='aspect-square shadow-md rounded flex flex-col p-4' key={`${collection.itemId}`}>
-                    <div className='w-full border flex items-center justify-center'>
-                      <img className='h-36' src={`${collection.image}`} alt={collection.name} />
-                    </div>
-                    <div className='my-3 flex flex-1 justify-center flex-col'>
-                      <div className='text-gray-700 text-lg mb-2 font-medium'>{collection.name}</div>
-                      <div className='text-gray-600 text-sm'>{collection.description}</div>
-                      {/* <div className='w-2/3 px-4 rounded-full py-2 bg-black flex-col text-slate-50 flex items-baseline justify-center'>
-                        <span className='text-lg mr-1 -mb-1'>{collection.totalRaised} ₺</span>
-                        <span className='text-xs text-slate-300'>toplam bağış</span>
-                      </div> */}
-                    </div>
-                    <div className=''>
-                      <span className='text-slate-700 text-sm font-semibold'>{collection.charityName}</span>
-                      <span className='text-slate-600 text-sm'> kampanyasıdır</span>
-                    </div>
+            collections.map((collection) => (
+              <a key={collection.itemId} href={`/collection?id=${collection.itemId}&nftAddress=${collection.nftAddress}`} className='block'>
+                <div className='flex flex-col gap-4 border-2 rounded-2xl p-4'>
+                  <img className='h-40 object-contain' src={`${collection.image}`} alt={collection.name} />
+                  <div className='text-lg sm:text-xl text-center text-slate-800 sm:truncate border-t-2'>
+                    {collection.name}
                   </div>
-                </a>
-              )
-            })
+                  <div className='p-4 rounded-md bg-[#343434] text-center text-[#fefefe]'>
+                    Detayları Gör
+                  </div>
+                  <div className='flex items-center'>
+                    <img className='w-8 h-8 rounded-full' src={collection.companyImage} alt={collection.charityName} />
+                    <span className='text-slate-700 text-sm ml-2 sm:truncate'><strong>{collection.charityName}</strong> kampanyasıdır</span>
+                  </div>
+                </div>
+              </a>
+            ))
           )
         }
       </div>
-    </div>
-  )
+    </>
+  );
 }
