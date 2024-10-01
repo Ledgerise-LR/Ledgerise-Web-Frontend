402a0186f0e76d6f749cce11e482f30d5621162a
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-10-01 18:52:16 +0300

    login update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,6 +7,7 @@ import { NotificationProvider } from "web3uikit";
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { GoogleTagManager } from '@next/third-parties/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function App({ Component, pageProps }) {
 
@@ -24,22 +25,24 @@ export default function App({ Component, pageProps }) {
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet"></link>
         <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
       </Head>
-      <MoralisProvider initializeOnMount={false}>
-        <NotificationProvider>
-          {
-            router.pathname != "/login" && router.pathname != "/register"
-              ? <Header />
-              : <div></div>
-          }
-          <Component {...pageProps} />
-          <GoogleTagManager gtmId='G-C50VJCBVP8' />
-          {
-            router.pathname != "/admin" && router.pathname != "/register" && router.pathname != "/login" && router.pathname != "/api-documentation"
-              ? <Footer />
-              : <div></div>
-          }
-        </NotificationProvider>
-      </MoralisProvider>
+      <GoogleOAuthProvider clientId="681279735908-g0er1l3ad3vto9ct23qrjnl46oq0sp10.apps.googleusercontent.com">
+        <MoralisProvider initializeOnMount={false}>
+          <NotificationProvider>
+            {
+              router.pathname != "/login" && router.pathname != "/register"
+                ? <Header />
+                : <div></div>
+            }
+            <Component {...pageProps} />
+            <GoogleTagManager gtmId='G-C50VJCBVP8' />
+            {
+              router.pathname != "/admin" && router.pathname != "/register" && router.pathname != "/login" && router.pathname != "/api-documentation"
+                ? <Footer />
+                : <div></div>
+            }
+          </NotificationProvider>
+        </MoralisProvider>
+      </GoogleOAuthProvider>;
     </div>
   )
 }
--- pages/login.jsx
+++ pages/login.jsx
@@ -4,11 +4,15 @@ import { useRouter } from 'next/router';
 import { AES } from "crypto-js";
 import { URL, PORT } from '@/serverConfig';
 import { FaHome } from 'react-icons/fa';
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+import { GoogleLogin } from '@react-oauth/google';
 
 export default function Home() {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [telephone, setTelephone] = useState("");
   const [success, setSuccess] = useState(null);
   const [successText, setSuccessText] = useState("");
   const [isLogin, setIsLogin] = useState(true);
@@ -72,7 +76,7 @@ export default function Home() {
   return (
     <div className='w-screen h-screen flex items-center justify-center'>
       <div className="flex max-sm:flex-col-reverse shadow-slate-600 sm:shadow-lg lg:rounded-2xl h-full w-full lg:w-4/5 xl:w-3/4 2xl:w-2/3 lg:h-3/4">
-        <a href='/' className='absolute max-sm:text-sm max-sm:right-4 max-sm:top-4 sm:m-6 z-20 py-2 px-2 xs:px-4 rounded bg-blue-500 w-fit text-slate-100 cursor-pointer'>
+        <a href='/' className='absolute max-sm:text-sm max-sm:right-4 max-sm:top-4 sm:m-6 z-20 py-2 px-2 xs:px-4 rounded bg-orange-400 text-sm w-fit text-black cursor-pointer'>
           <span className="hidden xs:inline">Ana Sayfa</span>
           <span className="inline xs:hidden">
             <FaHome size={20} />
@@ -82,7 +86,7 @@ export default function Home() {
           <div id="stars-content" className='absolute top-0 left-0 z-0 w-full h-full animate-pulse'></div>
           <div className='w-full flex flex-col items-center gap-3 my-auto'>
             <img className='h-24 xs:h-36 z-10 rounded border-2' src="icon.svg" alt="Ledgerise" />
-            <div className='text-slate-50 text-2xl lg:text-3xl xl:text-4xl'>Ledgerise'a Hoşgeldiniz</div>
+            <div className='text-slate-50 text-2xl lg:text-3xl xl:text-4xl'>Hoşgeldiniz!</div>
             <div className='text-slate-200 text-lg text-center px-0 max-sm:px-6'>
               {!isLogin ? "Halihazırda hesabınız var mı? Hemen bağış yapmak için giriş yapın." : "Hesabınız yok mu? İhtiyaç sahiplerine bağış yapabilmek için kayıt olun."}
             </div>
@@ -90,22 +94,43 @@ export default function Home() {
               {isLogin ? "Kayıt Ol" : "Giriş Yap"}
             </a>
           </div>
-          <div className='ml-auto text-slate-100 z-10'>Ledgerise 2023</div>
+          <div className='ml-auto text-slate-100 z-10'>LR 2023</div>
         </div>
 
         <div className={`flex flex-col p-8 justify-center max-sm:h-1/2 w-full sm:w-1/2 lg:rounded-2xl ${isLogin ? "" : "max-sm:translate-y-full sm:-translate-x-full"} transition-all duration-700 z-0`}>
-          <div className='text-2xl mb-8'>Ledgerise | {isLogin ? "Giriş Yap" : "Kayıt Ol"}</div>
-          <div className='mb-8'>
+          <div className='text-2xl mb-8 flex items-center'><img className='h-10' src="/logo.svg" alt="Logo" />&nbsp; | &nbsp;{isLogin ? "Giriş Yap" : "Kayıt Ol"}</div>
+          {/* <div className='mb-2'>
             {isLogin ? (
               <div><strong>Güvenli</strong> ve <strong>%100 şeffaf</strong>, <strong>uçtan uca</strong> bağış takip sistemine katılın.</div>
             ) : (
               "Şeffaf bağış sistemimize katılmak için aşağıdaki bilgileri doldurun."
             )}
+          </div> */}
+          <div className='mb-4'>
+            <GoogleLogin
+              onSuccess={(credentialResponse) => {
+                console.log(credentialResponse)
+              }}
+
+              onError={() => {
+                console.log("Login failed.")
+              }}
+            />
           </div>
           <div className='flex flex-col mb-4'>
-            <input className='bg-slate-100 rounded p-4 mb-4' type="text" placeholder='Email address'
+            <input className='bg-gray-50 rounded p-2 mb-4' type="text" placeholder='E-posta adresi'
               onChange={(e) => setEmail(e.target.value)} />
-            <input className='bg-slate-100 rounded p-4 mb-4' type="password" placeholder='Password'
+            <div>
+              <PhoneInput 
+                className='p-2 bg-slate-50 mb-4'
+                placeholder="Enter phone number"
+                value={telephone}
+                onChange={setTelephone}
+              />
+            </div>
+            <input className='bg-gray-50 rounded p-2 mb-4' type="password" placeholder='Şifre'
+              onChange={(e) => setPassword(e.target.value)} />
+            <input className='bg-gray-50 rounded p-2 mb-4' type="password" placeholder='Şifre tekrar'
               onChange={(e) => setPassword(e.target.value)} />
             {success !== null && (
               <div className={`${success ? "text-green-600" : "text-red-600"} mb-2`}>
@@ -116,7 +141,6 @@ export default function Home() {
               → {!isLogin ? "Kayıt Ol" : "Giriş Yap"}
             </div>
           </div>
-          <div className='ml-auto'>Powered by LR</div>
         </div>
       </div>
     </div>
